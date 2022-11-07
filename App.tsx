27ad7b4853c5d75a9636e9c4c117fb02deacd18e
bossmonkey98 <arunambregol007@gmail.com>
2022-11-07 13:28:33 +0530

    [feat]:screen routes defined

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View } from 'react-native';
+import { ThemeProvider ,useTheme } from './context/themeContext';
+import { WordProvider } from './context/WordContext';
+import Home from './Screens/Home';
+import Word from './Screens/Word';
+import {s} from 'react-native-wind'
+export type ScreenParamList = {
+  'Home': undefined;
+  'Word': undefined;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default function App() {
+  const Stack = createNativeStackNavigator<ScreenParamList>()
+  const { theme } = useTheme()
+  return (
+    <NavigationContainer>
+      <ThemeProvider>
+        <WordProvider>
+          <View style={s`h-full ${theme === 'dark' ? 'bg-black' : 'bg-white'}`}>
+            <Stack.Navigator initialRouteName='Home'>
+              <Stack.Screen name='Home' component={Home} options={{ title: 'Dictonary Home' }} />
+              <Stack.Screen name='Word' component={Word} options={{ title: 'Word Details' }} />
+            </Stack.Navigator>
+            <StatusBar style="auto" />
+          </View>
+        </WordProvider>
+      </ThemeProvider>
+    </NavigationContainer>
+
+  );
+}
